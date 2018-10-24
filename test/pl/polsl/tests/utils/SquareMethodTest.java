e35909a9c850e4c3352e105746ee3c60037caaff
Karol Kozuch <karol199714@gmail.com>
2018-10-24 14:44:05 +0200

    Added package-info for root package.
    Added some missing comments.

--- test/pl/polsl/tests/utils/SquareMethodTest.java
+++ test/pl/polsl/tests/utils/SquareMethodTest.java
@@ -6,16 +6,23 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**Contains tests for the {@link pl.polsl.utils.SquareMethod} class.
+ * @author Karol Kozuch Group 4 Section 8
+ * @version 1.0*/
 public class SquareMethodTest {
-    IntegralData integralData;
-    SquareMethod squareMethod;
+    /**An example integral to test*/
+    private IntegralData integralData;
+    /**Tested method object.*/
+    private SquareMethod squareMethod;
+    /**Initializes objects required for tests.*/
     @Before
     public void iniTests()
     {
         integralData = new IntegralData(0, 0.8);
         squareMethod = new SquareMethod(integralData, 100000);
     }
-
+    /**Tests calculateIntegral method. Will be passed if the returned by the method value
+     * fits in given range.*/
     @Test
     public void calculateIntegralTest()
     {
--- test/pl/polsl/tests/utils/TrapezoidMethodTest.java
+++ test/pl/polsl/tests/utils/TrapezoidMethodTest.java
@@ -6,17 +6,23 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**Contains tests for the {@link pl.polsl.utils.TrapezoidMethod} class.
+ * @author Karol Kozuch Group 4 Section 8
+ * @version 1.0*/
 public class TrapezoidMethodTest {
+    /**An example integral to test*/
     IntegralData integral;
+    /**Tested method object.*/
     TrapezoidMethod method;
-
+    /**Initializes objects required for tests.*/
     @Before
     public void initializeData()
     {
         integral= new IntegralData(0.0, 0.8);
         method =new TrapezoidMethod(integral, 1000000);
     }
-
+    /**Tests calculateIntegral method. Will be passed if the returned by the method value
+     * fits in given range.*/
     @Test
     public void calculateIntegralTest()
     {
